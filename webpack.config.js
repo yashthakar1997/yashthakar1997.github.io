db0cdb8eb1bcd0d24c4121df5e14bfc93e54408f
Yash Thakar <yashthakar1997@gmail.com>
2020-03-09 08:30:31 +0530

    added three js and webpack config for it

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'portfolio'),
+    filename: 'index.bundle.js'
+  }
+};
