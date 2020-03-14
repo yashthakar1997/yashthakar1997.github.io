2bc2d86af885c89a9a5380b7e6c8d067fdeb59f3
Yash Thakar <yashthakar1997@gmail.com>
2020-03-14 19:35:15 +0530

    Added lava effect

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  watch: true,
   output: {
     path: path.resolve(__dirname, 'portfolio'),
     filename: 'index.bundle.js'
